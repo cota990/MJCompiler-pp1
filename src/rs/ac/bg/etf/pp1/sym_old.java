20dbd00dc98d71d94ad8899226cf48a3048c10d2
cota990 <jovanovica990@gmail.com>
2020-02-20 12:53:53 +0100

    Update sym_old.java

--- src/rs/ac/bg/etf/pp1/sym_old.java
+++ src/rs/ac/bg/etf/pp1/sym_old.java
@@ -19,44 +19,51 @@ public class sym_old {
 	public static final int FOR = 13;
 	public static final int EXTENDS = 14;
 	public static final int CONTINUE = 15;
+	public static final int FOREACH = 16;
 	
 	// identifiers
 	
-	public static final int IDENT = 16;
+	public static final int IDENT = 17;
 	
 	// constants
 	
-	public static final int NUMBER = 17;
-	public static final int CHAR = 18;
-	public static final int BOOL = 19;
+	public static final int NUMBER = 18;
+	public static final int CHAR = 19;
+	public static final int BOOL = 20;
 	
 	// operators
 	
-	public static final int PLUS = 20;
-	public static final int MINUS = 21;
-	public static final int MUL = 22;
-	public static final int DIV = 23;
-	public static final int MOD = 24;
-	public static final int EQUALS = 25;
-	public static final int NOT_EQUALS = 26;
-	public static final int GREATER_THAN = 27;
-	public static final int GREATER_THAN_EQUALS = 28;
-	public static final int LESS_THAN = 29;
-	public static final int LESS_THAN_EQUALS = 30;
-	public static final int AND = 31;
-	public static final int OR = 32;
-	public static final int ASSIGN = 33;
-	public static final int INC = 34;
-	public static final int DEC = 35;
-	public static final int SEMICOLON = 36;
-	public static final int COMMA = 37;
-	public static final int PERIOD = 38;
-	public static final int LEFT_PARENTHESIS = 39;
-	public static final int RIGHT_PARENTHESIS = 40;
-	public static final int LEFT_BRACKET = 41;
-	public static final int RIGHT_BRACKET = 42;
-	public static final int LEFT_BRACE = 43;
-	public static final int RIGHT_BRACE = 44;
+	public static final int PLUS = 21;
+	public static final int MINUS = 22;
+	public static final int MUL = 23;
+	public static final int DIV = 24;
+	public static final int MOD = 25;
+	public static final int PLUSASSIGN = 26;
+	public static final int MINUSASSIGN = 27;
+	public static final int MULASSIGN = 28;
+	public static final int DIVASSIGN = 29;
+	public static final int MODASSIGN = 30;
+	public static final int EQUALS = 31;
+	public static final int NOT_EQUALS = 32;
+	public static final int GREATER_THAN = 33;
+	public static final int GREATER_THAN_EQUALS = 34;
+	public static final int LESS_THAN = 35;
+	public static final int LESS_THAN_EQUALS = 36;
+	public static final int AND = 37;
+	public static final int OR = 38;
+	public static final int ASSIGN = 39;
+	public static final int INC = 40;
+	public static final int DEC = 41;
+	public static final int SEMICOLON = 42;
+	public static final int COMMA = 43;
+	public static final int PERIOD = 44;
+	public static final int COLON = 45;
+	public static final int LEFT_PARENTHESIS = 46;
+	public static final int RIGHT_PARENTHESIS = 47;
+	public static final int LEFT_BRACKET = 48;
+	public static final int RIGHT_BRACKET = 49;
+	public static final int LEFT_BRACE = 50;
+	public static final int RIGHT_BRACE = 51;
 	
 	// eof symbol
 	
